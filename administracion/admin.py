2e83de065e885b28bae34f5a731d625dd06c4530
Enrique Galvez <kikega@telefonica.net>
2020-04-16 18:56:04 +0200

    Arreglo de login con imagen asociación

--- administracion/admin.py
+++ administracion/admin.py
@@ -3,18 +3,19 @@ from django.contrib import admin
 from .models import *
 
 class ExamenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'evento', 'alumno', 'grado')
+    list_display = ('evento', 'alumno', 'grado')
+    raw_id_fields = ('alumno',)
     list_filter = ('evento',)
 
 
 class AlumnoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'apellidos', 'nombre', 'dojo', 'grado')
+    list_display = ('apellidos', 'nombre', 'dojo', 'grado')
     list_filter = ('dojo',)
     raw_id_fields = ('dojo',)
     search_fields = ('nombre', 'apellidos')
 
 class CursilloAdmin(admin.ModelAdmin):
-    list_display = ('id', 'fecha', 'evento', 'ciudad')
+    list_display = ('fecha', 'evento', 'ciudad')
     list_filter = ('ciudad',)
     date_hierarchy = 'fecha'
     filter_horizontal = ('alumnos',)
