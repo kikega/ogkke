18060bcae6ad5c4b7f6b634eb4d358eee4d55e91
Enrique Galvez <kikega@telefonica.net>
2020-05-21 01:35:40 +0200

    Arreglo pagina inicio

--- administracion/views.py
+++ administracion/views.py
@@ -9,8 +9,13 @@ from administracion.models import Dojo, Alumno, Examen, Cursillo
 
 @login_required
 def index(request):
+    """Página principal de la aplicación, nos mostrará un resúmen con datos globales"""
     cn = Alumno.objects.all().count()
-    return render(request, 'index.html', {'cn':cn})
+    danes = list()
+    for i in range(1, 9):
+        i = Alumno.objects.filter(grado=i).count()
+        danes.append(i)
+    return render(request, 'index.html', {'cn':cn, 'danes':danes})
 
 def login_view(request):
     """Vista para login en aplicación"""
@@ -29,6 +34,7 @@ def login_view(request):
 
 @login_required
 def logout_view(request):
+    """Vista para hacer logout"""
     logout(request)
     return redirect('login')
 
